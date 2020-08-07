a068e488fd0514ed411f8d59abdfa000288c3210
bela25 <belakhairiyah@gmail.com>
2020-08-07 14:19:47 +0700

    plus folder web

---
+++ database/migrations/2020_08_04_132444_verifikasi.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Verifikasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verifikasis', function (Blueprint $table) {
+            $table->increments('idverifikasi');
+            $table->date('tanggal');
+            $table->enum('status',['menikah','belum_menikah']);
+            $table->string('ktp');
+            $table->string('kk');
+            $table->string('npwp');
+            $table->date('tgl_diterima');
+            $table->unsignedInteger('pegawai');
+            $table->foreign('pegawai')->references('nip')->on('pegawais');
+            $table->unsignedInteger('customer');
+            $table->foreign('customer')->references('idcustomers')->on('customers');
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verifikasis');
+        //
+    }
+}
